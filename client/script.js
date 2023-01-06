c5dee7107ab3a6a03e2af3af48038000e43150ef
2XBC <software@doublebcreative.com>
2023-01-06 14:44:36 -0500

    Changed url again at fetch.

--- client/script.js
+++ client/script.js
@@ -87,8 +87,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    //const response = await fetch(svcLocation, {
-    const response = await fetch('https://twoxbc-cgpt-service.onrender.com', {        
+    const response = await fetch('https://twoxbc-cgpt-service.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
